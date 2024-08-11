10fde471da1d40b25d0821a8d6ff7b75e0dc3ca5
b1994mi <b1994mi@gmail.com>
2024-08-11 19:03:10 +0700

    add validation if user is not found

--- handler/user/find_handler.go
+++ handler/user/find_handler.go
@@ -1,15 +1,23 @@
 package user
 
 import (
+	"errors"
+
+	"github.com/b1994mi/golang-rest-api-example/util"
+	"gorm.io/gorm"
 )
 
 func (h *handler) FindHandler(req *request) (any, error) {
-	m, err := h.userRepo.FindOneBy(map[string]interface{}{
+	user, err := h.userRepo.FindOneBy(map[string]interface{}{
 		"id": req.ID,
 	})
-	if err != nil {
+	if err != nil && !errors.Is(err, gorm.ErrRecordNotFound) {
 		return nil, err
 	}
 
-	return m, nil
+	if user == nil {
+		return nil, util.New404Res("user with id %v is not found", req.ID)
+	}
+
+	return user, nil
 }
